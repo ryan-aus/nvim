853c9c9125cd05a967c979cd162a7d08bc3c8b4d
git@ryanwarnock.me <lebronse@mb.local>
2021-12-03 19:46:15 +0800

    changed to nvim-cmp

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,13 +1,12 @@
 require('packer').startup(function(use)
 	use 'wbthomason/packer.nvim'
 	use 'neovim/nvim-lspconfig'
-	use 'nvim-lua/completion-nvim'
+	use 'hrsh7th/nvim-cmp'
 	use {
 		'nvim-treesitter/nvim-treesitter',
 		run = ':TSUpdate'
 	}
 	use 'SirVer/ultisnips'
-	use 'honza/vim-snippets'
 	use 'lifepillar/vim-gruvbox8'
 	use 'NLKNguyen/papercolor-theme'
 	use 'sheerun/vim-polyglot'
@@ -15,6 +14,11 @@ require('packer').startup(function(use)
 	use 'kyazdani42/nvim-tree.lua'
 	use 'junegunn/fzf.vim'
 	use 'junegunn/fzf'
+  use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
+  use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
+  use 'L3MON4D3/LuaSnip' -- Snippets plugin
+  use 'akinsho/flutter-tools.nvim'
+  use 'nvim-lua/plenary.nvim'
 end)
 
 vim.o.completeopt = "menuone,noselect"
@@ -24,7 +28,7 @@ require'nvim-tree'.setup {}
 
 require'lspconfig'.elixirls.setup{
 	cmd = { "/Users/lebronse/source/elixir/elixir-ls/language_server.sh" };
-	on_attach = require'completion'.on_attach;
+	on_attach = require'cmp'.on_attach;
 }
 
 require'nvim-treesitter.configs'.setup{
@@ -35,9 +39,76 @@ require'nvim-treesitter.configs'.setup{
 	},
 }
 
+require("flutter-tools").setup{} -- use defaults
+
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
     vim.lsp.diagnostic.on_publish_diagnostics, {
         virtual_text = false
     }
 )
 
+-- Add additional capabilities supported by nvim-cmp
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+
+
+local lspconfig = require('lspconfig')
+
+-- Enable some language servers with the additional completion capabilities offered by nvim-cmp
+local servers = { 'clangd', 'rust_analyzer', 'pyright', 'tsserver' }
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    -- on_attach = my_custom_on_attach,
+    capabilities = capabilities,
+  }
+end
+
+-- Set completeopt to have a better completion experience
+vim.o.completeopt = 'menuone,noselect'
+
+-- luasnip setup
+local luasnip = require 'luasnip'
+
+-- nvim-cmp setup
+local cmp = require 'cmp'
+cmp.setup {
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
+    end,
+  },
+  mapping = {
+    ['<C-p>'] = cmp.mapping.select_prev_item(),
+    ['<C-n>'] = cmp.mapping.select_next_item(),
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.close(),
+    ['<CR>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    },
+    ['<Tab>'] = function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end,
+    ['<S-Tab>'] = function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end,
+  },
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+  },
+}
